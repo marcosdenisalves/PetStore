c8d0ac0bb768073c090655e4d915e6ca37b217b4
Study Profile <dee.alves2@gmail.com>
2021-06-03 18:37:55 -0300

    Criação de autenticacao, cabecalho e rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RodapeModule } from './componentes/rodape/rodape.module';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
 import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -13,7 +15,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { UsuarioService } from './usuario/usuario.service';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 const url = 'http://localhost:3000/user/login';
 
@@ -8,12 +10,24 @@ const url = 'http://localhost:3000/user/login';
   providedIn: 'root',
 })
 export class AutenticacaoService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  autenticar(usuario: String, senha: string) :Observable<any>{
-    return this.httpClient.post(url, {
-      userName: usuario,
-      password: senha,
-    });
+  autenticar(usuario: String, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient.post(
+      url,
+      {
+        userName: usuario,
+        password: senha,
+      },
+      { observe: 'response' }
+    ).pipe(
+      tap((res) => {
+        const authToken = res.headers.get('x-access-token') ?? '';
+        this.usuarioService.salvaToken(authToken);
+      })
+    )
   }
 }
